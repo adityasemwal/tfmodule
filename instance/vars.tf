dbc3a24a707457fab7b4fa5c718b924a9c54c610
aditya semwal <aditya.semwal@nagarro.com>
2020-03-05 05:14:36 +0000

    AMIS variable removed from vars.tf

--- instance/vars.tf
+++ instance/vars.tf
@@ -1,12 +1,6 @@
 variable "AWS_REGION"{
   default = "us-west-1"
 }
-
-variable "AMIS"{
-  default = {
-    us-west-1 = "ami-01c94064639c71719"
-  }
-}
 variable "COUNT"{}
 variable "AWS_INSTANCE"{
   default = "t2.micro"
